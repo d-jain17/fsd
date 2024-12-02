ba3e9e158aba1f7a89ff66a125b801c2b3cfe83c
d-jain17 <j.deepali2004@gmail.com>
2024-12-02 11:38:04 +0530

    conflicts resolved

--- React-Work/src/App.jsx
+++ React-Work/src/App.jsx
@@ -1,14 +1,17 @@
 import "./App.css";
 import Student from "./components/Student/Student";
+import logo from "./assets/logo.jpg"
 
 function App() {
-  const data = { name: "shdkshf", rollno: "132", branch: "CSE" };
+  const data = { name: "Aman", rollno: "15", branch: "CSE" };
   return (
     <>
+      pic={<img src={logo} height={100} width={100}/>}
       <div className="college-header">
         ABES Engineering College
       </div>
       <Student name={data.name} rollno={data.rollno} branch={data.branch} />
+      
     </>
   );
 }
