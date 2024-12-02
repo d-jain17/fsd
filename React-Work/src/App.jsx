7d955a435ee6ae8df90c3f14a8ba1dbcf2f14918
d-jain17 <j.deepali2004@gmail.com>
2024-12-02 11:44:25 +0530

    usestate created

--- React-Work/src/App.jsx
+++ React-Work/src/App.jsx
@@ -1,18 +1,24 @@
 import "./App.css";
 import Student from "./components/Student/Student";
 import logo from "./assets/logo.jpg"
+import StudentState from "./components/Student/studentState/StudentState";
 
 function App() {
-  const data = { name: "Aman", rollno: "15", branch: "CSE" };
+  const data = { name: "yaya", rollno: "15", branch: "CSE" };
   return (
-    <>
-      pic={<img src={logo} height={100} width={100}/>}
-      <div className="college-header">
-        ABES Engineering College
-      </div>
-      <Student name={data.name} rollno={data.rollno} branch={data.branch} />
+    // {/*
+    // <>
+    //   pic={<img src={logo} height={100} width={100}/>}
+    //   <div className="college-header">
+    //     ABES Engineering College
+    //   </div>
+    //   <Student name={data.name} rollno={data.rollno} branch={data.branch} />
       
-    </>
+    // </>
+    // */}
+    <div>
+      <StudentState/>
+    </div>
   );
 }
 
---
+++ React-Work/src/components/Student/studentState/StudentState.jsx
@@ -0,0 +1,6 @@
+import React, {useState} from "react";
+const StudentState= () =>{
+const [count, setCount] = useState(100);
+return <div>{count}</div>
+};
+ export default StudentState;
