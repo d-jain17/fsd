f19004e4dea27af518ee2473aeb53f48c6a304b1
d-jain17 <j.deepali2004@gmail.com>
2024-12-02 12:05:24 +0530

    increment decrement added

--- React-Work/src/components/Student/studentState/StudentState.jsx
+++ React-Work/src/components/Student/studentState/StudentState.jsx
@@ -1,6 +1,24 @@
 import React, {useState} from "react";
 const StudentState= () =>{
 const [count, setCount] = useState(100);
-return <div>{count}</div>
+function increment(){
+    setCount(count+4)
+}
+function decrement(){
+    setCount(count-10)
+}
+return (
+    <div>
+<div>{count}</div>
+<div>
+    <button onClick={increment}>Do Increment</button>
+</div>
+<div>
+<button onClick={decrement}>Do Decrement</button>
+
+</div>
+</div>
+)
+
 };
  export default StudentState;
