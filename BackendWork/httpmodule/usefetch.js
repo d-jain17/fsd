1e2c0d2fc916ea6f972afb15897328c40b059922
d-jain17 <j.deepali2004@gmail.com>
2025-02-25 10:16:56 +0530

    till 25th feb

--- BackendWork/httpmodule/usefetch.js
+++ BackendWork/httpmodule/usefetch.js
@@ -15,24 +15,42 @@ const server = http.createServer(async (req,res)=>{
         .container{
             background-color: floralwhite;
             border: 2px solid black;
-            display: flex;
-            flex-wrap: wrap;
+            width:300px;
             justify-content: space-around;
         }
-        .child1,.child2,.child3{
+        img{
             height: 200px;
             width: 300px;
             border: 1px solid sienna;
 
 
         }
+            .parent{
+            display:flex;
+            flex-wrap:wrap;
+            justify-content:space-between;
+            gap:10px;}
+    
     </style>
 </head>
 <body>
-    <div class="container">
-        <div class="child1"> ${jsondata[0].category} <img height="200px" width="300px" src=${jsondata[0].image}></div>
-        <div class="child2"> ${jsondata[1].category} <img height="200px" width="300px" src=${jsondata[1].image}></div>
-        <div class="child3"> ${jsondata[2].category} <img height="200px" width="300px" src=${jsondata[2].image}></div>
+    <div class="parent">
+    ${
+        jsondata.map(ele=>{
+            return(
+                `
+                <div class="container">
+                <div>${ele.id}</div>
+                <div><img src="${ele.image}"></div>
+                <div>${ele.category}
+                ${ele.id}</div>
+                <div>${ele.description}</div>
+                <div>Rs. ${ele.price}</div>
+                </div>
+                `
+            )
+        })
+    }
     </div>
 </body>
 </html>`
