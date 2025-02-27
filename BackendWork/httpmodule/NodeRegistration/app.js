d45c0368f8bc37139f0a986363612f5c5cebaec4
d-jain17 <j.deepali2004@gmail.com>
2025-02-27 13:07:21 +0530

    till 27th

--- BackendWork/httpmodule/NodeRegistration/app.js
+++ BackendWork/httpmodule/NodeRegistration/app.js
@@ -12,9 +12,40 @@ const server= http.createServer((req,res)=>{
         req.on('end', async()=>{
             const {name,email,password}=JSON.parse(body)
             console.log(name)
-            await fs.appendFile('student.json',JSON.stringify({name,email,password}))
+            const fdata= await fs.readFile('student.json',{encoding:'utf-8'})
+            arr=JSON.parse(fdata)
+            console.log(arr)
+            const result=arr.find(ele=>ele.email==email)
+            if(result){
+                res.setHeader('Content-Type','application/json')
+                return res.end(JSON.stringify({"message":"This is already registered with us"}))
+            }
+            arr.push({name,email,password})
+            await fs.writeFile('student.json',JSON.stringify(arr,null,2))
+            res.end(JSON.stringify({"message":"/register api hit successfully!"}))
         })
-        res.end(JSON.stringify({"message":"/register api hit successfully!"}))
+    }
+    else if(req.url=="/login" && req.method=="POST"){
+        let body=""
+        let arr=[]
+        req.on("data",chunk=>{
+            body+=chunk
+        })
+        req.on('end',async()=>{
+            const {email,password}=JSON.parse(body)
+            const fdata= await fs.readFile('student.json',{encoding:'utf-8'})
+            arr=JSON.parse(fdata)
+            console.log(arr)
+            const result=arr.find(ele=>ele.email==email && ele.password==password)
+            if(result){
+                res.setHeader('Content-Type','application/json')
+                return res.end(JSON.stringify({"message":"Login Success"}))
+            }
+            else{
+                res.setHeader('Content-Type','application/json')
+                return res.end(JSON.stringify({"message":"Login Success"}))
+            }
+        })
     }
 //res.end("Welcome to Node Server")
 })
